2e1f057995b78d032b5d9cea196974be8c40d7ae
RobertSoare9 <soare.robert9@gmail.com>
2023-05-03 19:33:03 +0300

    finished the game following an youtube tutorial

--- myScript.js
+++ myScript.js
@@ -23,7 +23,7 @@ function checkWinner(playerSelection,computerSelection){
 
 function playRound(playerSelection, computerSelection){
  const result= checkWinner(playerSelection, computerSelection)
- if(result == " tie"){
+ if(result == "tie"){
     return "It's a Tie";
  }
  else if (result == "win"){
@@ -33,7 +33,45 @@ function playRound(playerSelection, computerSelection){
     return `You lose ${computerSelection} beats ${playerSelection}`;
  }
 }
-const playerSelection = "rock";
-
-const computerSelection=getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false)
+    {
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower= choice.toLowerCase();
+        if (array.includes(choiceInLower)){
+            return choiceInLower;
+        }
+    }
+}
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome")
+    for (let i=0;i<5;i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+        if(checkWinner(playerSelection,computerSelection == "Player"))
+        {
+            scorePlayer++;
+        }
+        else if(checkWinner(playerSelection,computerSelection)=="Computer"){
+            scoreComputer++;
+        }
+        console.log("Game Over");
+        if(scorePlayer > scoreComputer){
+            console.log("Player Won");
+        }
+        if (scoreComputer > scorePlayer){
+            console.log("Computer Won");
+        }
+        else{
+            console.log ("Tie");
+        }
+    }
+}
+game()
